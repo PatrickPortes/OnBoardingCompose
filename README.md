bd4c6dc9879bdf8c519f510091cd91dd8a18fc5d
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2023-12-21 11:04:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OnBoardingCompose
+ This project integrates onboarding screens into the Android app using Jetpack Compose.
---
+++ app/src/main/java/com/example/onboardingcompose/navigation/Screen.kt
@@ -0,0 +1,8 @@
+package com.example.onboardingcompose.navigation
+
+sealed class Screen(val route: String) {
+
+    object SplashScreen : Screen(route = "splash_screen")
+    object HomeScreen : Screen(route = "home_screen")
+
+}
